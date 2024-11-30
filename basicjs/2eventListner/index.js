256a6432cf4f34e4f5c8583b38b63babd0da26f1
pratik kagale <pratikkagale3333@gmail.com>
2024-11-30 13:59:39 +0530

    event listner

--- basicjs/2eventListner/index.js
+++ basicjs/2eventListner/index.js
@@ -1,12 +1,33 @@
 
-let paras=document.querySelectorAll('p');
+// let paras=document.querySelectorAll('p');
 
-for (let i = 0; i< paras.length; i++) 
-    {
-          let para = paras[i];
-          para.addEventListener('click',function () {
+// fetching all p tag one by one
+// for (let i = 0; i< paras.length; i++) 
+//     {
+//           let para = paras[i];
+//           para.addEventListener('click',function () {
 
-            alert("You clicked on para : "+ (i+1));
-          })
+//             alert("You clicked on para : "+ (i+1));
+//           })
           
+// }
+
+function alertpara(event) {
+
+    alert("You have clicked on para: "+event.target.textContent);
+    
 }
+
+
+// for (let i = 0; i < paras.length; i++) {
+    
+//     let para=paras[i];
+//     para.addEventListener('click',alertpara);
+    
+// }
+
+
+
+//catching wrapper id
+let mydiv=document.getElementById('wrapper');
+document.addEventListener('click',alertpara)
